17443b8f8182bf59609470a0be1c92bb30a670cb
NguyenQuocHuy643 <huy_dpm245533@student.agu.edu.vn>
2025-09-23 01:28:08 +0700

    giai câu6_chuong2

---
+++ cau6_chuong2.py
@@ -0,0 +1,7 @@
+> / (Phép chia) dùng để trả về kết quả của phép chia thông thường, bao gồm cả phần thập phân
+// (Chia lấy phần nguyên) dùng để trả về phần nguyên của thương số, làm tròn xuống số nguyên gần nhất
+% (Chia lấy phần dư) dùng để trả về phần dư của phép chia
+** (Lũy thừa) dùng để tính lũy thừa của một số cho một số mũ
+and: Trả về True nếu cả hai điều kiện đúng, ngược lại trả về false
+or: Trả về True nếu ít nhất một điều kiện đúng, chỉ trả về False khi cả hai đều sai
+is: Kiểm tra hai biến có cùng tham chiếu đến cùng một đối tượng trong bộ nhớ hay không
